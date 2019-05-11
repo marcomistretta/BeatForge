5a6bf6b23c8fa887c8fa7464575b26f7489adf63
misterm <marco.mistretta@stud.unifi.it>
2019-05-11 17:51:31 +0200

    Scheletro del progetto

---
+++ Metronome.cpp
@@ -0,0 +1,11 @@
+//
+// Created by misterm on 11/05/19.
+//
+
+#include "Metronome.h"
+
+void Metronome::beep() {
+
+}
+
+
---
+++ Player.cpp
@@ -0,0 +1,45 @@
+//
+// Created by misterm on 11/05/19.
+//
+
+#include "Player.h"
+
+void Player::play(const Metronome &metronome) {
+
+}
+
+void Player::play(const Pattern &pattern) {
+
+}
+
+void Player::play(const Pattern &pattern, int index) {
+
+}
+
+void Player::play(const Percussions &percussions) {
+
+}
+
+void Player::stop(const Metronome &metronome) {
+
+}
+
+void Player::stop(const Pattern &pattern) {
+
+}
+
+void Player::stop(const Pattern &pattern, int index) {
+
+}
+
+void Player::stop(const Percussions &percussions) {
+
+}
+
+void Player::pause() {
+
+}
+
+void Player::scroll() {
+
+}
