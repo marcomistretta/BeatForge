244246371dd8191aa46799747a6e66141315ff68
Marco Mistretta <49488524+marcomistretta@users.noreply.github.com>
2019-05-11 18:32:58 +0200

    Commit test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "Metronome.h"
+#include "Pattern.h"
+#include "Player.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "test" << std::endl;
     return 0;
 }
